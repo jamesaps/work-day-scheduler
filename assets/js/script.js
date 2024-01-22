3e4f25a7ea59bffad48fde4edb8c9e18bd2b542f
James Stott <japstott@gmail.com>
2024-01-22 20:39:21 +0000

    update function to get date as a string to include appropriate ordinal suffix according to current day of month

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
 function getCurrentDayString() {
-    var date = dayjs();
-    var dayString = date.format('dddd, MMMM D');
+    var date = dayjs(); // instantiate date object to current date/time
+    var dayString = date.format('dddd, MMMM D'); // formats date in following manner: Thursday, September 5 
+    var dayOfMonth = date.date(); // gets day of month from dayjs() object
+    var ordinalSuffix = getOrdinalSuffix(dayOfMonth); // get appropriate suffix according to date (st, nd, rd, th)
+
+    dayString += ordinalSuffix;
 
     return dayString;
+}
+
+function getOrdinalSuffix(number) {
+    if (number % 10 === 1 && number % 100 != 11) {
+        return 'st';
+    }
+
+    if (number % 10 === 2 && number % 100 != 12) {
+        return 'nd';
+    }
+
+    if (number % 10 === 3 && number % 100 != 13) {
+        return 'rd';
+    }
+
+    return 'th';
 }
